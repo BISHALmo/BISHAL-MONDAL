16c6e2d633ff5a51e46c6c90e343e642d04cc38b
BISHALmo <91148407+BISHALmo@users.noreply.github.com>
2022-01-11 20:54:02 +0530

    Add files via upload

---
+++ volume_of_sphere.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    float v;
+    printf(" Enter the redious of sphere : ");
+    scanf("%d",&r);
+    v=(4*3.14*r*r*r)/3;
+    printf("%f",v);
+    return 0;
+}
